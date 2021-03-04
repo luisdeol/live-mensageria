db13a36e12c9e60ba9b95dfb6b3430394412a8ae
Luis Felipe <luisfelipekai@gmail.com>
2021-03-04 20:10:41 -0300

    Finalizando Live #11.

---
+++ LiveMensageria/Controllers/MessagesController.cs
@@ -0,0 +1,60 @@
+﻿using LiveMensageria.InputModels;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LiveMensageria.Controllers
+{
+    [Route("api/messages")]
+    public class MessagesController : ControllerBase
+    {
+        private const string QUEUE_NAME = "messages";
+        private readonly ConnectionFactory _factory;
+        public MessagesController()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage([FromBody] SendMessageInputModel sendMessageInputModel)
+        {
+            // #somostodosdto
+
+            using (var connection = _factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // Declara a fila para que caso ela não exista ainda, eu crie ela
+                    channel.QueueDeclare(
+                        queue: QUEUE_NAME,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                        );
+
+                    // Formatar os dados para envio para a fila
+                    var stringMessage = JsonSerializer.Serialize(sendMessageInputModel);
+                    var byteArray = Encoding.UTF8.GetBytes(stringMessage);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: QUEUE_NAME,
+                        basicProperties: null,
+                        body: byteArray
+                        );
+                }
+            }
+
+            return Accepted();
+        }
+    }
+}
---
+++ LiveMensageria/InputModels/SendMessageInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LiveMensageria.InputModels
+{
+    public class SendMessageInputModel
+    {
+        public string FromUser { get; set; }
+        public string ToUser { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
